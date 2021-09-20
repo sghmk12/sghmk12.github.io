176ef1153146cc5f69369b90d2c7b49bed0311e6
Sathira <sathirakatugaha@gmail.com>
2021-09-20 00:17:11 -0400

    redesign

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,4 @@
-jQuery(document).ready(function($) {
-    var initialScrollPos = $(window).scrollTop()
-    if (initialScrollPos > 48) $(".x-navbar").addClass("alt-color");
+import {Gradient} from "./Gradient.js"
 
-    $(window).scroll(function() {
-      var scrollPos = $(window).scrollTop(),
-          navbar = $('.x-navbar');
-  
-      if (scrollPos > 48) {
-          navbar.addClass('alt-color');
-      } else {
-          navbar.removeClass('alt-color');
-      }
-    });
-
-    $(".navbar-toggler").click(function(){
-        var scrollPos = $(window).scrollTop()
-        if (scrollPos <= 48) $(".x-navbar").toggleClass("alt-color");
-        $(".x-navbar").toggleClass("expanded");
-    })
-});
+const gradient = new Gradient()
+gradient.initGradient('#gradient-canvas')
---
+++ javascript/navbarScrollEffect.js
@@ -0,0 +1,28 @@
+const ALT_COLOUR_SCROLL_POS = 24;
+
+function getScrollPos () {
+    return window.pageYOffset || document.documentElement.scrollTop;
+}
+
+jQuery(document).ready(function($) {
+    var initialScrollPos = getScrollPos();
+    console.log({initialScrollPos})
+    if (initialScrollPos > ALT_COLOUR_SCROLL_POS) $(".x-navbar").addClass("alt-color");
+
+    window.onscroll = function() {
+        var scrollPos = getScrollPos(),
+            navbar = $('.x-navbar');
+                
+        if (scrollPos > ALT_COLOUR_SCROLL_POS) {
+            navbar.addClass('alt-color');
+        } else {
+            navbar.removeClass('alt-color');
+        }
+    };
+
+    $(".navbar-toggler").click(function(){
+        var scrollPos = getScrollPos();
+        if (scrollPos <= ALT_COLOUR_SCROLL_POS) $(".x-navbar").toggleClass("alt-color");
+        $(".x-navbar").toggleClass("expanded");
+    })
+});
